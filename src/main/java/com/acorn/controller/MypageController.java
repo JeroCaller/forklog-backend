1e3f607834208016c12af25e214ec3890d77e43a
rmk <qop589@naver.com>
2024-12-31 18:40:21 +0900

    fix:환경 수정

--- src/main/java/com/acorn/controller/MypageController.java
+++ src/main/java/com/acorn/controller/MypageController.java
@@ -1,11 +1,12 @@
 package com.acorn.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -30,7 +31,7 @@ public class MypageController {
 	@GetMapping("/review/member/{no}")
 	public ResponseEntity<Object> getReviewsByMemberNo(
 				@RequestParam(value = "page", required = false, defaultValue = "1" ) int page,
-				@PathVariable("no") String memberNo){
+				@PathVariable("no") String memberNo,Authentication authentication){
 		Page<ReviewResponseDto> list= reviewsProcess.getReviewsByMemberNo(memberNo,page-1);
 		Map<String, Object> map = new HashMap<>();
 		map.put("list", list);
@@ -41,9 +42,12 @@ public class MypageController {
 	//파일 목록 받기 (@RequestParam("files") List<MultipartFile> files): 여러 파일을 한 번에 받을 수 있도록
 	//List<MultipartFile> 타입으로 받습니다. 프론트엔드에서 여러 파일을 files[]로 전송해야 합니다.
 	//formData에 reviewNo 없이 보내야 합니다.
-	@PostMapping
-	public ResponseEntity<String> registerReview(@ModelAttribute("inputDto") ReviewRequestDto inputDto,
+	@PostMapping("/review")
+	public ResponseEntity<String> registerReview(@ModelAttribute ReviewRequestDto inputDto, Authentication authentication,
 												@RequestParam(value = "files", required = false) List<MultipartFile> files) throws Exception {
+	    if (files == null) {
+	        files = new ArrayList<>();
+	    }
 		reviewsProcess.registReview(inputDto, files);
 		return ResponseEntity.ok("리뷰 등록에 성공했습니다.");
 	}
@@ -51,14 +55,17 @@ public class MypageController {
 	//formData에 reviewNo 넣어서 보내야 합니다.
 	@PutMapping("/review/{no}")
 	public ResponseEntity<String> updateReview(@ModelAttribute("inputDto") ReviewRequestDto inputDto,
-					@PathVariable("no") String reviewNo,
+					@PathVariable("no") String reviewNo,Authentication authentication,
 					@RequestParam(value = "files", required = false) List<MultipartFile> files) throws Exception {
+	    if (files == null) {
+	        files = new ArrayList<>();
+	    }
 		reviewsProcess.updateReview(reviewNo,inputDto, files);
 		return ResponseEntity.ok("리뷰 수정에 성공했습니다.");
 	}
 	//별점 리뷰 삭제
 	@DeleteMapping("/review/{no}")
-	public ResponseEntity<String> deleteReview(@PathVariable("no") String reviewNo)throws Exception{
+	public ResponseEntity<String> deleteReview(@PathVariable("no") String reviewNo,Authentication authentication)throws Exception{
 		reviewsProcess.deleteReview(reviewNo);
 		return ResponseEntity.ok("리뷰 삭제에 성공했습니다.");
 	}
