2efa4aa7bd8aa56209cf8b0f6494d4fb9df4d7d5
EaseHee <geonhee@naver.com>
2025-01-05 03:09:16 +0900

    refact: 웹소켓 회원 정보 조회 요청 API 작성

--- src/main/java/com/acorn/controller/ChatsController.java
+++ src/main/java/com/acorn/controller/ChatsController.java
@@ -8,15 +8,23 @@ import org.springframework.web.bind.annotation.*;
 
 import com.acorn.dto.ChatsRequestDto;
 import com.acorn.dto.ChatsResponseDto;
+import com.acorn.dto.MembersDto;
 import com.acorn.process.ChatsProcess;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+// @RequestMapping은 @MessageMapping API 경로를 
 public class ChatsController {
 
     private final ChatsProcess chatsProcess;
+    
+    // 채팅방 이용 회원 정보 조회
+    @GetMapping("/chat/member")
+    public ResponseEntity<MembersDto> getMember() {
+    	return ResponseEntity.ok().body(chatsProcess.getMember());
+    }
 
     // 모든 채팅 메시지 조회
     @GetMapping("/chat/message")
